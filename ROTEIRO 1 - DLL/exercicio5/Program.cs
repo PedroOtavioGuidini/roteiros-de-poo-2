d48f1d5e8e8e1d95c023eee90cb4b07dd1225c78
Pedro Otávio Guidini <109149557+PedroOtavioGuidini@users.noreply.github.com>
2026-04-06 10:49:22 -0300

    Add files via upload

---
+++ ROTEIRO 1 - DLL/exercicio5/Program.cs
@@ -0,0 +1,24 @@
+﻿using BibliotecaGeometria;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var geo = new Geometria();
+
+        Console.WriteLine("Calculadora de Geometria ");
+
+        Console.Write("Digite o raio para calcular a área do círculo: ");
+        if (double.TryParse(Console.ReadLine(), out double r))
+            Console.WriteLine($"Área do círculo: {geo.AreaCirculo(r):F4}\n");
+
+        Console.Write("Digite a largura do retângulo: ");
+        double.TryParse(Console.ReadLine(), out double l);
+
+        Console.Write("Digite a altura do retângulo: ");
+        double.TryParse(Console.ReadLine(), out double h);
+
+        Console.WriteLine($"Área do retângulo: {geo.AreaRetangulo(l, h):F4}");
+        Console.WriteLine($"Perímetro do retângulo: {geo.PerimetroRetangulo(l, h):F4}");
+    }
+}
