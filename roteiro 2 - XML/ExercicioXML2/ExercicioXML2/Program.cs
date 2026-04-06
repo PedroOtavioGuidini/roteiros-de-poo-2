2c585f907e431d3c8f81c10c30284f01d1e5e495
Pedro Otávio Guidini <109149557+PedroOtavioGuidini@users.noreply.github.com>
2026-04-06 11:15:16 -0300

    Add files via upload

---
+++ roteiro 2 - XML/ExercicioXML2/ExercicioXML2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+class Program
+{
+    static void Main()
+    {
+        var lista = new ListaProdutos
+        {
+            Produtos = new List<Produto>
+            {
+                new Produto { Nome = "Headset",    Preco = 199.90 },
+                new Produto { Nome = "Teclado Mecânico", Preco = 349.90 },
+                new Produto { Nome = "Mouse\"",   Preco = 1299.90 }
+            }
+        };
+
+        string caminho = "produtos.xml";
+
+        XmlSerializer serializer = new XmlSerializer(typeof(ListaProdutos));
+        using (StreamWriter writer = new StreamWriter(caminho))
+        {
+            serializer.Serialize(writer, lista);
+        }
+
+       
+        string conteudo = File.ReadAllText(caminho);
+        Console.WriteLine(conteudo);
+    }
+}
